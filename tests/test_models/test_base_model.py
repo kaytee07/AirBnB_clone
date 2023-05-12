e24ddfd55f24023f5ca83d4e569689dd689d001e
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-05-12 02:44:28 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,65 @@
+#!/usr/bin/python3
+"""At this point We build unittests for models/base_model.py.
+Unittest classes inherits unittest.TestCase:
+    TestBaseModel_instantiation
+    TestBaseModel_save
+    TestBaseModel_to_dict
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
 
+class TestBaseModel_to_dict(unittest.TestCase):
+    """Unittest methods to test to_dict method of the BaseModel class."""
+
+    def test_to_dict_type(self):
+        base_model = BaseModel()
+        self.assertTrue(dict, type(base_model.to_dict()))
+
+    def test_to_dict_has_correct_keys(self):
+        base_model = BaseModel()
+        self.assertIn("id", base_model.to_dict())
+        self.assertIn("created_at", base_model.to_dict())
+        self.assertIn("updated_at", base_model.to_dict())
+        self.assertIn("__class__", base_model.to_dict())
+
+    def test_to_dict_has_added_attributes(self):
+        base_model = BaseModel()
+        base_model.name = "My First Model"
+        base_model.my_number = 89
+        self.assertIn("name", base_model.to_dict())
+        self.assertIn("my_number", base_model.to_dict())
+
+    def test_to_dict_datetime_attributes_are_strs(self):
+        base_model = BaseModel()
+        base_model_dict = base_model.to_dict()
+        self.assertEqual(str, type(base_model_dict["created_at"]))
+        self.assertEqual(str, type(base_model_dict["updated_at"]))
+
+    def test_to_dict_output(self):
+        date_time = datetime.today()
+        base_model = BaseModel()
+        base_model.id = "119427"
+        base_model.created_at = bm.updated_at = dt
+        to_dict = {
+            'id': '119427',
+            '__class__': 'BaseModel',
+            'created_at': date_time.isoformat(),
+            'updated_at': date_time.isoformat()
+        }
+        self.assertDictEqual(base_model.to_dict(), to_dict)
+
+    def test_contrast_to_dict_against_dict(self):
+        base_model = BaseModel()
+        self.assertNotEqual(base_model.to_dict(), base_model.__dict__)
+
+    def test_to_dict_with_arg(self):
+        bm = BaseModel()
+        with self.assertRaises(TypeError):
+            bm.to_dict(None)
+
+
+if __name__ == "__main__":
+    unittest.main()
