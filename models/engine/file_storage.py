b97f3e2ce9b1781a59a7aaf1f91a953e972398a5
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-05-14 05:53:15 +0100

    Correct the f"" to string.format

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,7 +20,7 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        key = f"{obj.__class__.__name__}.{obj.id}"
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
