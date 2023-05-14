e6f4dc0f5e1b73dc874d815c36f55cfd6a0b4957
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-05-14 02:58:53 +0100

    Removed the comment from created at

--- models/base_model.py
+++ models/base_model.py
@@ -11,8 +11,8 @@ class BaseModel:
   
   def __init__(self, *args, **kwargs):
     """this method is to initiali """
-    #self.id = str(uuid.uuid4())
-    #self.created_at = datetime.now()
+    self.id = str(uuid.uuid4())
+    self.created_at = datetime.now()
     self.updated_at = datetime.now()
     
     if len(kwargs) != 0:
