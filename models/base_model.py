f102c8a987146476cbf47e3b89ccd5810817f26c
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-05-14 06:29:23 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,8 @@ class BaseModel:
                 else:
                     if key == "created_at" or key == "updated_at":
                         # Parse the string into a datetime object
-                        datetime_obj = datetime.datetime.strptime(value)
-                        value = datetime.datetime.isoformat(datetime_obj)
+                        #datetime_obj = datetime.datetime.strptime(value)
+                        value = datetime.datetime.isoformat(value)
                         setattr(self, key, value)
                     else:
                         setattr(self, key, value)
