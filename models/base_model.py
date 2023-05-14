d6a548f937f110dc3538df21cfb0a2dc1ae0b06f
kaytee07 <kofi.taylor07@gmail.com>
2023-05-14 04:16:22 +0000

    3 to 7 redo

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,45 @@
 #!/usr/bin/python3
-"""Base Model Class for all attributes and methods for other classes
-"""
+"""this script contains the the base_model class for objects"""
+
 
 import uuid
 import datetime
-from __init__ import storage
+from . import storage
+
 
 class BaseModel:
-  """defines all common attributes/methods for other classes: """
-  
-  def __init__(self, *args, **kwargs):
-    """this method is to initiali """
-    self.id = str(uuid.uuid4())
-    self.created_at = datetime.now()
-    self.updated_at = datetime.now()
-    
-    if len(kwargs) != 0:
-      for key, value in kwargs.items():
-        if key == "created_at" || key =="updated_at"
-        self.key = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
-        elif key == "__class__":
-          continue
+    """defines all common attributes/methods for other classes: """
+    def __init__(self, *args, **kwargs):
+        self.updated_at = datetime.datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                else:
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.datetime.fromisoformat(value)
+                        setattr(self, key, value)
+                    else:
+                        setattr(self, key, value)
         else:
-          self.key = value
-      else:
-        storage.new()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        
-  def __str__(self):
-    class_n=self.__class__.__name___
-    return f"class_n {self.id} {self.__dict__} "
-  
-  def save(self):
-    storage.save()
-    self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            storage.new(self)
 
-  def to_dict(self): 
-    instance_dictionary=self.__dict__
-    instance_dictionary['__class__']=self.__class__.__name__
-    self.created_at = isoformat(self.created_at)
-    self.updated_at = isoformat(self.updated_at)
-    for key, value in instance_dictionary.items():
-      if isinstance(value, (str, int, float, bool)):
-        instance_dictionary[key]=value
-    
-    return instance_dictionary
+    def __str__(self):
+        class_n = self.__class__.__name__
+        return f"{class_n} {self.id} {self.__dict__}"
 
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        instance_dictionary = self.__dict__
+        instance_dictionary['__class__'] = self.__class__.__name__
+        instance_dictionary["created_at"] = self.created_at.isoformat()
+        instance_dictionary["updated_at"] = self.updated_at.isoformat()
+        for key, value in instance_dictionary.items():
+            if isinstance(value, (str, int, float, bool)):
+                instance_dictionary[key] = value
+        return instance_dictionary
